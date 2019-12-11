5c475b06f5671a74792c496459efcd8ee1241515
daiyuanchao <1572094763@qq.com>
2019-12-11 16:16:05 +0800

    Initial commit

---
+++ src/main/java/com/dychao/forcastserver/ForcastServerApplication.java
@@ -0,0 +1,13 @@
+package com.dychao.forcastserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForcastServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForcastServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dychao/forcastserver/ForcastServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dychao.forcastserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForcastServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
